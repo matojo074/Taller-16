e6ed1564aadf4d770b53c3b62c1e0863a1f5a5c4
Mateo Cevallos <mateo.cevallos@epn.edu.ec>
2020-07-23 16:31:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+void ingresar (int k[], int num);
+void burbujapares (int k[], int num, int l[]);
+void imprimir (int k[], int num);
+
+int main() 
+{
+  int arreglo [50];
+  int num;
+  cout << "Ingrese el tamanio deseado para el arreglo: ";
+  cin >> num;
+  ingresar (arreglo, num);
+  cout <<endl;
+  burbujapares(arreglo, num);
+  cout <<endl;
+  imprimir(arreglo, num);
+  cout <<endl;
+  return 0;
+}
+
+void ingresar (int k[], int num)
+{
+  int i;
+  for(i=0;i<num;i++)
+    {
+        cout<<"Ingrese num["<< i << "]: ";
+        cin>>k[i];
+    }
+}
+
+void burbujapares (int k[], int num, int l[])
+{
+  int i;
+  for (i=0; i<num; i++)
+  {
+    if (k[i]%2==0)
+    {
+      l[num]=k[i];
+      num++;
+
+    }
+  }
+
+}
+
+void imprimir (int k[], int num)
+{
+  int i;
+  for(i=0;i<num;i++)
+  cout << "Los numeros pares son: " <<endl;
+    {
+        cout<< k[i] <<endl;
+    }
+}
